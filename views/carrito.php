01a1ccea994b3e8105c207d0fdb01911bff9fef4
ISRA-VR <israelvalerdi65@gmail.com>
2025-08-19 13:22:33 -0600

    agrege menu.js en carrito

--- views/carrito.php
+++ views/carrito.php
@@ -448,6 +448,7 @@ if (isset($_GET['success'])) {
         }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../public/js/menu.js"></script>
 </body>
 
 </html>
